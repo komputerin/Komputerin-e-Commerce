5b5587b791145c94fdf41589899cd8eb4e324527
Dahlia Annisa <dahlia2699@gmail.com>
2019-11-28 00:23:22 +0700

    modifikasi tampilan beranda di folder layout/main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -13,23 +13,56 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">LaravelApp</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="/">Home</a>
-                    <a class="nav-item nav-link" href="/about">About</a>
-                    <a class="nav-item nav-link" href="/mahasiswa">Mahasiswa</a>
-                    <a class="nav-item nav-link" href="/students">Students</a>
-                    <a class="nav-item nav-link" href="/admin">Admin</a>
-                </div>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+    <div class="container">
+        <h3><i class="fas fa-cart-plus text-success"></i></h3>
+        <a class="navbar-brand font-weight-bold" href="{{url('/layout')}}"><img src="{{url('img/cau.png')}}">KOMPUTER.IN</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Kategori
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Processor</a>
+                <a class="dropdown-item" href="#">Motherboard</a>
+                <a class="dropdown-item" href="#">VGA</a>
+                <a class="dropdown-item" href="#">RAM</a>
+                <a class="dropdown-item" href="#">SSD</a>
+                <a class="dropdown-item" href="#">Hardisk</a>
+                <a class="dropdown-item" href="#">Water Cooler</a>
+                <a class="dropdown-item" href="#">Power Suply</a>
+                <a class="dropdown-item" href="#">Casing</a>
+                <a class="dropdown-item" href="#">FAN</a>
+              </div>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Status Pemesanan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pembayaran</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cara Pemesanan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Masuk</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </div>
-    </nav>
+        </div>
+      </nav>
 
     @yield('container')
 
